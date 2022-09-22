b3fd2371855753236e4c1f43d6d98bdbd7ad1cc6
carlos juatan <>
2022-09-22 17:53:50 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width= device-width, initial-scale=1.0"> -->
+
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    
+    <title>News Site Development</title>
+  </head>
+  <body>
+
+    <div id="header">Games</div> <!-- End of header div -->
+
+    <div id="menu">
+      <ul> <!-- <ul> is used to create lists -->
+        <li><a href="home"    >HOME    </a></li> <!-- <li> serves as a line in lists -->
+        <li><a href="tunning" >RUNNING </a></li>
+        <li><a href="games"   >GAMES   </a></li>
+        <li><a href="cycling" >CYCLING </a></li>
+        <li><a href="football">FOOTBALL</a></li>
+        <li><a href="contact" >CONTACT </a></li>
+      </ul>
+    </div> <!-- End of menu div -->
+
+    <?php
+      echo $_GET['url'];
+      echo "aloha";
+    ?>
+
+    <div id="footer">
+      All rights reserved.
+    </div> <!-- End of 'div name' div -->
+  </body>
+</html>
---
+++ php/home.php
@@ -0,0 +1,62 @@
+    <div id="highlights">
+      <div id="highlights1"> <img src="img/1.jpg"> </div>
+      <div id="highlights2">
+        <div id="highlightsA"> <img src="img/2.jpg"> </div>
+        <div id="highlightsB"> <img src="img/3.webp"> </div>
+      </div>
+    </div> <!-- End of highlights div -->
+
+    <div id="center">
+      <div id="pictures">
+        <h2>Pictures</h2>
+        <hr>
+        <img id="picture1" src="img/4.jpg">
+        <img id="picture2" src="img/5.jpg">
+        <img id="picture3" src="img/6.jpg">
+        <ul id="image-list" >
+          <li> <img src="img/7.jpg"> </li>
+          <li> <img src="img/8.jpg"> </li>
+          <li> <img src="img/9.jpg"> </li>
+          <li> <img src="img/10.jpg"> </li>
+          <li> <img src="img/11.jpg"> </li>
+        </ul>
+      </div> <!-- End of pictures div -->
+
+      <div id="game-list">
+        <h2>Games</h2>
+        <hr>
+        <ul>
+          <li>FIFA 21</li>
+          <li>EA Sports 21</li>
+          <li>League of Legends</li>
+          <li>Counter Strike</li>
+          <li>Fortnite</li>
+          <li>Overwatch</li>
+          <li>Free Fire</li>
+        </ul>
+      </div> <!-- End of game-list div -->
+      
+    </div> <!-- End of center div -->
+
+    <div id="news">
+      <h2>News</h2>
+      <hr>
+
+      <div id="news1">
+        <img src="img/7.jpg">
+        <div class="news-title">Set the news title 1 here</div>
+        Nonono nonono onno onnonono nonononono nnonono
+        ononnoon onnonono nononono nnonoonno onnoon onnono onno
+        onno onnoon nnoonon nn
+        <a href="">Leia Mais...</a>
+      </div><!-- End of news1 div -->
+
+      <div id="news2">
+        <img src="img/8.jpg">
+        <div class="news-title">Set the news title 2 here</div>
+        Nonono nonono onno onnonono nonononono nnonono
+        ononnoon onnonono nononono nnonoonno onnoon onnono onno
+        onno onnoon nnoonon nn
+        <a href="">Leia Mais...</a>
+      </div><!-- End of news1 div -->
+    </div> <!-- End of news div -->
